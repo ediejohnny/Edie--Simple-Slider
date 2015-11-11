ec3b5fcbec5b3edbec64bdba2482c1bdbf98c9f8
Edie Johnny <edie.eu@gmail.com>
2015-11-11 11:49:11 -0200

    Update

--- javascripts/ess.js
+++ javascripts/ess.js
@@ -4,6 +4,7 @@
 var cooldown     = $(".edie-slides").data("cooldown");
 var width        = $(".edie-slides").data("width") || 0;
 var height       = $(".edie-slides").data("height") || 0;
+var position     = $(".edie-slides").data("position") || "100%";
 var currentIndex = 0;
 var nextIndex    = 0;
 var slides       = [];
@@ -11,10 +12,9 @@ $(".edie-slides-container .edie-slide").each(function(index){
 	$(this).css({
 		"background"          : "url('" + $(this).data("src") + "') no-repeat",
 		"background-position" : "top center",
-		"background-size"     : "cover",
+		"background-size"     : position,
 		"width"               : "100%",
-		"height"              : (height > 0 ? height + "px" : $(".edie-slides").parent().innerHeight() + "px"),
-		"visibility"          : "hidden",
+		"padding-top"         : (height > 0 ? height + "px" : $(".edie-slides").parent().innerHeight() + "px"),
 		"display"             : "none"
 	});
 	slides[index] = $(this);
@@ -30,18 +30,18 @@ var initEdieSlider = function(nextIndex) {
 	if(slides.length > 0) {
 		var current = setTimeout(function(){
 			$(".edie-slides-container .edie-slide").each(function(index){
-				if($(this).css("visibility") == "visible") {
+				if($(this).css("display") != "none") {
 					currentIndex = index;
 				}
 			});
 			slides[currentIndex].fadeOut(500, "swing", function(){
-				slides[currentIndex].css("visibility", "hidden");
-				slides[nextIndex].hide().css("visibility", "visible").fadeIn();
+				slides[currentIndex].css("display", "none");
+				slides[nextIndex].hide().fadeIn();
 			});
 			initEdieSlider(nextIndex + 1);
 		}, cooldown * 1000);
 	}
 }
 
-slides[0].hide().css("visibility", "visible").fadeIn();
+slides[0].fadeIn();
 var current = initEdieSlider(1);
